07401818437330a5b2dfb2c41a66f4476c9895fa
cichy <cichy.shebang@gmail.com>
2021-10-06 21:36:29 +0200

    calculator api completed

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ app = Flask(__name__)
 api = Api(app)
 
 def check(postedData, method):
-    if method == 'add':
+    if method in ('add', 'subtract', 'multiply'):
         if 'x' not in postedData or 'y' not in postedData:
             return 301
         else:
+            return 200
+    else: 
+        if method == 'divide':
+            if 'x' not in postedData or 'y' not in postedData:
+                return 301
+            elif int(postedData['y']) == 0:
+                return 302
             return 200
 
 class Add(Resource):
@@ -36,14 +43,76 @@ class Add(Resource):
         }
         return jsonify(retMap)
 
+
 class Subtract(Resource):
-    pass
+    def post(self):
+
+        postedData = request.get_json()
+
+        statusCode = check(postedData, 'subtract')
+
+        if statusCode != 200:
+            retJson = {
+                "Message": "An error happened",
+                'Status Code': statusCode
+            }
+            return jsonify(retJson)
+
+        x = int(postedData['x'])
+        y = int(postedData['y'])
+        res = x-y
+        retMap = {
+            'Message': res,
+            'Status Code': 200
+        }
+        return jsonify(retMap)
+
 
 class Multiply(Resource):
-    pass
+    def post(self):
+    
+        postedData = request.get_json()
+
+        statusCode = check(postedData, 'multiply')
+
+        if statusCode != 200:
+            retJson = {
+                "Message": "An error happened",
+                'Status Code': statusCode
+            }
+            return jsonify(retJson)
+
+        x = int(postedData['x'])
+        y = int(postedData['y'])
+        res = x*y
+        retMap = {
+            'Message': res,
+            'Status Code': 200
+        }
+        return jsonify(retMap)
 
 class Divide(Resource):
-    pass
+    def post(self):
+        
+        postedData = request.get_json()
+
+        statusCode = check(postedData, 'divide')
+
+        if statusCode != 200:
+            retJson = {
+                "Message": "An error happened",
+                'Status Code': statusCode
+            }
+            return jsonify(retJson)
+
+        x = float(postedData['x'])
+        y = int(postedData['y'])
+        res = x/y
+        retMap = {
+            'Message': res,
+            'Status Code': 200
+        }
+        return jsonify(retMap)
 
 
 @app.route('/')
@@ -51,6 +120,9 @@ def hello_world():
     return('Hello World!')
 
 api.add_resource(Add, "/add")
+api.add_resource(Subtract, "/subtract")
+api.add_resource(Multiply, "/multiply")
+api.add_resource(Divide, "/divide")
 
 if __name__ == '__main__':
     app.run(debug=True)
