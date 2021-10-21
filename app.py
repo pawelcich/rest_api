bad16b7c5a5830f55c4aaa0e08eb39e6da166e94
cichy <cichy.shebang@gmail.com>
2021-10-21 22:18:35 +0200

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from enum import Enum
@@ -36,4 +37,4 @@ async def operate(operation: Operation, item: Item):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
