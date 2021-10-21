255c2aa5850276d77cb8dabacc6ed3b12d53f484
cichy <cichy.shebang@gmail.com>
2021-10-21 22:14:49 +0200

    rewritten into fastapi

--- app.py
+++ app.py
@@ -1,128 +1,39 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
 
 
-app = Flask(__name__)
-
-api = Api(app)
-
-def check(postedData, method):
-    if method in ('add', 'subtract', 'multiply'):
-        if 'x' not in postedData or 'y' not in postedData:
-            return 301
-        else:
-            return 200
-    else: 
-        if method == 'divide':
-            if 'x' not in postedData or 'y' not in postedData:
-                return 301
-            elif int(postedData['y']) == 0:
-                return 302
-            return 200
-
-class Add(Resource):
-    def post(self):
-        
-        postedData = request.get_json()
-
-        statusCode = check(postedData, 'add')
-
-        if statusCode != 200:
-            retJson = {
-                "Message": "An error happened",
-                'Status Code': statusCode
-            }
-            return jsonify(retJson)
-
-        x = int(postedData['x'])
-        y = int(postedData['y'])
-        res = x+y
-        retMap = {
-            'Message': res,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
+class Item(BaseModel):
+    item_1: int
+    item_2: int
 
 
-class Subtract(Resource):
-    def post(self):
-
-        postedData = request.get_json()
-
-        statusCode = check(postedData, 'subtract')
-
-        if statusCode != 200:
-            retJson = {
-                "Message": "An error happened",
-                'Status Code': statusCode
-            }
-            return jsonify(retJson)
-
-        x = int(postedData['x'])
-        y = int(postedData['y'])
-        res = x-y
-        retMap = {
-            'Message': res,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
+class Operation(str, Enum):
+    add = 'add'
+    subtract = 'subtract'
+    multiply = 'multiply'
+    divide = 'divide'
 
 
-class Multiply(Resource):
-    def post(self):
-    
-        postedData = request.get_json()
-
-        statusCode = check(postedData, 'multiply')
-
-        if statusCode != 200:
-            retJson = {
-                "Message": "An error happened",
-                'Status Code': statusCode
-            }
-            return jsonify(retJson)
-
-        x = int(postedData['x'])
-        y = int(postedData['y'])
-        res = x*y
-        retMap = {
-            'Message': res,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
-
-class Divide(Resource):
-    def post(self):
-        
-        postedData = request.get_json()
-
-        statusCode = check(postedData, 'divide')
-
-        if statusCode != 200:
-            retJson = {
-                "Message": "An error happened",
-                'Status Code': statusCode
-            }
-            return jsonify(retJson)
-
-        x = float(postedData['x'])
-        y = int(postedData['y'])
-        res = x/y
-        retMap = {
-            'Message': res,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
+app = FastAPI()
 
 
-@app.route('/')
-def hello_world():
-    return('Hello World!')
+@app.get("/")
+async def root():
+    return {"message": "Welcome to basic math operations api !"}
 
-api.add_resource(Add, "/add")
-api.add_resource(Subtract, "/subtract")
-api.add_resource(Multiply, "/multiply")
-api.add_resource(Divide, "/divide")
+
+@app.post("/{operation}", tags=['Basic math operations'])
+async def operate(operation: Operation, item: Item):
+    if operation.value == 'add':
+        return {"result": item.item_1 + item.item_2}
+    if operation.value == 'subtract':
+        return {"result": item.item_1 - item.item_2}
+    if operation.value == 'multiply':
+        return {"result": item.item_1 * item.item_2}
+    if operation.value == 'divide':
+        return {"result": item.item_1 / item.item_2}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
